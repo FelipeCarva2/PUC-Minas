3c31a900615f5d0ed856fae5097a2fbf9661eead
Felipe Gabriel de Carvalho <fg7318089@gmail.com>
2025-04-08 21:49:09 -0300

    Alterações tp aeds

--- AEDS2/tp02/Show.java
+++ AEDS2/tp02/Show.java
@@ -1,6 +1,26 @@
+// ---------------------------------------------------------------------------------------------------- //
+
+// Imports
 import java.util.*;
+import java.util.Scanner;
+import java.util.UUID;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+// ---------------------------------------------------------------------------------------------------- //
 
 public class Show{
+    // Global variables
+    //public static final String FILE_PATH = "/tmp/disneyplus.csv";
+    public static final String FILE_PATH = "/home/felipe/PUCMinas/AEDS2/tp02/disneyplus.csv";
+    public static ArrayList<Show> allShows = new ArrayList<Show>();
+
     //Atributos
     private String Show_id;
     private String type;
@@ -46,92 +66,93 @@ public class Show{
     }
 
     // Getters e Setters
-    public String getShow_id() {
-        return Show_id;
+    public String getShow_id() {return Show_id;}
+    public void setShow_id(String show_id) { Show_id = show_id;}
+    
+    public String getType() {return type;}
+    public void setType(String type) {this.type = type;}
+    
+    public String getTitle() {return title;}
+    public void setTitle(String title) {this.title = title;}
+    
+    public String getDirector() {return director;}
+    public void setDirector(String director) {this.director = director;}
+    
+    public String[] getCast() {return cast;}
+    public void setCast(String[] cast) {this.cast = cast;}
+    
+    public String getCountry() {return country;}
+    public void setCountry(String country) {this.country = country;}
+    
+    public Date getDate_added() {return date_added;}
+    public void setDate_added(Date date_added) {this.date_added = date_added;}
+    
+    public int getRelease_year() {return release_year;}
+    public void setRelease_year(int release_year) {this.release_year = release_year;}
+    
+    public String getRating() {return rating;}
+    public void setRating(String rating) {this.rating = rating;}
+    
+    public String getDuration() {return duration;}
+    public void setDuration(String duration) {this.duration = duration;}
+    
+    public String[] getListed_in() {return listed_in;}
+    public void setListed_in(String[] listed_in) {this.listed_in = listed_in;}  
+
+    //Função ler arquivo csv
+    public static ArrayList<Show> Ler(){
+        String linha = "", conteudo = "";
+        String[] s = new String[200];
+		int aux = 0;//teste
+		ArrayList <Show> show = new ArrayList<>();
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(FILE_PATH));
+			while ((linha = br.readLine()) != null) {
+                
+                //teste
+				if(aux==1){
+                    int j=0;
+                for(int i=0; i< linha.length(); i++){
+                    
+                    char c = linha.charAt(i);
+                    if(c == '"'){
+                        while(c != '"'){
+                            c = linha.charAt(i);
+                            s[j]= s[j]+c;
+
+                        }j++; i++;
+                    }else if(c==','){
+                        s[j]="NaN";
+                    }else{
+                        while(c!=','){
+                            c = linha.charAt(i);
+                            s[j]= s[j]+c;  
+                        }
+                    }
+                }
+                    for(int i =0; i< 10; i++){
+                        System.out.println(s[j]);
+
+                    }
+                }
+                aux++;
+                //teste
+                
+			}
+		} catch (Exception e) {
+			System.out.println("Erro!!");
+		}
+		return show;
     }
 
-    public void setShow_id(String show_id) {
-        Show_id = show_id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public String[] getCast() {
-        return cast;
-    }
-
-    public void setCast(String[] cast) {
-        this.cast = cast;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public Date getDate_added() {
-        return date_added;
-    }
-
-    public void setDate_added(Date date_added) {
-        this.date_added = date_added;
-    }
-
-    public int getRelease_year() {
-        return release_year;
-    }
-
-    public void setRelease_year(int release_year) {
-        this.release_year = release_year;
-    }
-
-    public String getRating() {
-        return rating;
-    }
-
-    public void setRating(String rating) {
-        this.rating = rating;
-    }
-
-    public String getDuration() {
-        return duration;
-    }
-
-    public void setDuration(String duration) {
-        this.duration = duration;
-    }
-
-    public String[] getListed_in() {
-        return listed_in;
-    }
-
-    public void setListed_in(String[] listed_in) {
-        this.listed_in = listed_in;
+    public static void main(String[] args){
+        String conteudo = "";
+      
+            //Scanner sc = new Scanner(System.in);
+            ArrayList <Show> show = Ler();
+        
+    
     }
 }
 
@@ -140,7 +161,3 @@ public class Show{
 
 
 
-/*public static void main(String[] args){
-
-
-}*/
