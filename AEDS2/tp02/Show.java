b50091aa4f4aaa5b964935b33e89c06db14e08fc
Felipe Gabriel de Carvalho <fg7318089@gmail.com>
2025-04-09 19:31:04 -0300

    Implementando leitura

--- AEDS2/tp02/Show.java
+++ AEDS2/tp02/Show.java
@@ -1,14 +1,8 @@
 // ---------------------------------------------------------------------------------------------------- //
-
 // Imports
-import java.util.*;
 import java.util.Scanner;
-import java.util.UUID;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Date;
 import java.text.SimpleDateFormat;
@@ -33,6 +27,7 @@ public class Show{
     private String rating; 
     private String duration; 
     private String[] listed_in; 
+    
 
     // Construtor padrão
     public Show() {
@@ -98,61 +93,126 @@ public class Show{
     
     public String[] getListed_in() {return listed_in;}
     public void setListed_in(String[] listed_in) {this.listed_in = listed_in;}  
+// ---------------------------------------------------------------------------------------------------- //
+//Funções
 
+    //Função transformar uma string em um objeto
+    public static Show TrasInObj(String linha){
+        Show showzinho = new Show();
+        int i=0;        
+        ArrayList<String> atrib = new ArrayList<>();
+       
+        while (i<linha.length()) {
+            //Testa se um conjunto de dados
+            if(linha.charAt(i)=='"'){
+                 i++;
+                 StringBuilder aux = new StringBuilder();
+                 while (i+1<linha.length()) {
+
+                      //testo se é fim do conjunto, se for para, se não continua a leitura
+                     if(linha.charAt(i) == '"'){
+                         if (linha.charAt(i+1) == ','){
+                            i++;
+                            break;
+                         }
+                        i++;
+                    }
+
+                    aux.append(linha.charAt(i));
+					i++;                       
+                    }
+                    atrib.add(aux.toString()); 
+            //Se não for um conjunto de dados realiza esse else              
+            }else{
+            //Testa se é um campo vazio ,, NaN
+                 if (i < linha.length() && linha.charAt(i) == ',') {
+				    if(linha.charAt(i+1) == ','){
+					        atrib.add("NaN");
+		            }
+                    i++;
+                //Se não cair em nenhma das condições anteriores quer dizer que é um dado unitario a ser lido
+                }else{
+                    StringBuilder aux = new StringBuilder();
+					while(i < linha.length() && linha.charAt(i) != ','){
+						aux.append(linha.charAt(i));
+						i++;
+					}
+                    atrib.add(aux.toString());
+                }
+            }
+
+            
+        }
+        //teste para ver se todos os atributos foram separados em strings
+        //for (int j = 0; j < atrib.size(); j++){ System.out.println(atrib.get(j)); } System.out.println("\n");
+        
+        showzinho.setShow_id(atrib.get(0));
+		showzinho.setType(atrib.get(1));
+		showzinho.setTitle(atrib.get(2));
+		showzinho.setDirector(atrib.get(3));
+		//showzinho.setCast(splitArrays(atrib.get(4)));
+		showzinho.setCountry(atrib.get(5));
+		//showzinho.setDate_added(atrib.get(6));
+        try {
+            if(!atrib.get(6).equals("NaN")) {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM d, yyyy"); 
+                showzinho.setDate_added(dateFormat.parse(atrib.get(6)));
+            } 
+        } catch (Exception e) {
+            System.err.println("Erro ao converter data: " + atrib.get(6));
+            showzinho.setDate_added(null);
+        }
+		showzinho.setRelease_year(Integer.parseInt(atrib.get(7)));
+		showzinho.setRating(atrib.get(8));
+		showzinho.setDuration(atrib.get(9));
+		//showzinho.setLISTED_IN(atrib.get(10));
+		
+         
+
+
+
+        return showzinho;
+    }
     //Função ler arquivo csv
     public static ArrayList<Show> Ler(){
-        String linha = "", conteudo = "";
-        String[] s = new String[200];
-		int aux = 0;//teste
-		ArrayList <Show> show = new ArrayList<>();
+        String linha = "";
+		int aux = 0;
+		ArrayList <Show> show = new ArrayList<>(); 
 
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(FILE_PATH));
 			while ((linha = br.readLine()) != null) {
+
+                if(aux >= 1){
+					System.out.println(linha + '\n');
+					show.add(TrasInObj(linha));
+				}
+
+				aux++;
                 
-                //teste
-				if(aux==1){
-                    int j=0;
-                for(int i=0; i< linha.length(); i++){
-                    
-                    char c = linha.charAt(i);
-                    if(c == '"'){
-                        while(c != '"'){
-                            c = linha.charAt(i);
-                            s[j]= s[j]+c;
-
-                        }j++; i++;
-                    }else if(c==','){
-                        s[j]="NaN";
-                    }else{
-                        while(c!=','){
-                            c = linha.charAt(i);
-                            s[j]= s[j]+c;  
-                        }
-                    }
-                }
-                    for(int i =0; i< 10; i++){
-                        System.out.println(s[j]);
-
-                    }
-                }
-                aux++;
-                //teste
                 
 			}
+            br.close();
 		} catch (Exception e) {
 			System.out.println("Erro!!");
 		}
 		return show;
+        
     }
 
+    //---------------------------------------------------------------------------------------------------- //
+    //Fução main 
     public static void main(String[] args){
-        String conteudo = "";
-      
-            //Scanner sc = new Scanner(System.in);
-            ArrayList <Show> show = Ler();
-        
     
+      
+        Scanner sc = new Scanner(System.in);
+        ArrayList <Show> show = Ler();
+        System.out.println("Digite o id para retornar o titulo:");
+		int id = sc.nextInt();
+		System.out.println(show.get(id - 1).title);
+        
+           
+        sc.close();
     }
 }
 
