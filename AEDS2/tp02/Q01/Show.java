369979a992caf8242eda3c0d92b8823d9abc20f6
Felipe Gabriel de Carvalho <fg7318089@gmail.com>
2025-04-10 21:39:10 -0300

    QUESTAO 1 ESTA FEITAgit status ou quase...

--- AEDS2/tp02/Q01/Show.java
+++ AEDS2/tp02/Q01/Show.java
@@ -1,32 +1,31 @@
 // ---------------------------------------------------------------------------------------------------- //
 // Imports
-import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.util.*;
 
 // ---------------------------------------------------------------------------------------------------- //
 
 public class Show{
     // Global variables
-    //public static final String FILE_PATH = "/tmp/disneyplus.csv";
-    public static final String FILE_PATH = "/home/felipe/PUCMinas/AEDS2/tp02/disneyplus.csv";
-    public static ArrayList<Show> allShows = new ArrayList<Show>();
+    public static final String FILE_PATH = "/tmp/disneyplus.csv";
+    //public static final String FILE_PATH = "/home/felipe/PUCMinas/AEDS2/tp02/Q01/disneyplus.csv";
+    //public static ArrayList<Show> allShows = new ArrayList<Show>();
 
     //Atributos
     private String Show_id;
     private String type;
     private String title;
     private String director;
-    private String[] cast; 
+    private ArrayList<String> cast = new ArrayList<>();
     private String country;
     private Date date_added;
     private int release_year;
     private String rating; 
     private String duration; 
-    private String[] listed_in; 
+    private ArrayList<String> listed_in = new ArrayList<>(); 
     
 
     // Construtor padrão
@@ -35,30 +34,29 @@ public class Show{
         this.type = "";             
         this.title = "";            
         this.director = "";         
-        this.cast = null;             
+        this.cast = new ArrayList<>();           
         this.country = "";          
         this.date_added = null;      
         this.release_year = 0;        
         this.rating = "";           
         this.duration = "";         
-        this.listed_in = null;        
+        this.listed_in = new ArrayList<>();       
     }
 
     // Construtor com parâmetros
-    public Show(String show_id, String type, String title, String director, String[] cast, String country,
-                Date date_added, int release_year, String rating, String duration, String[] listed_in) {
+    public Show(String show_id, String type, String title, String director, ArrayList<String> cast, String country, Date date_added, int release_year, String rating, String duration, ArrayList<String> listed_in) {
         this.Show_id = show_id;
         this.type = type;
         this.title = title;
         this.director = director;
-        this.cast = cast;
+        this.cast = new ArrayList<>(cast);
         this.country = country;
-        this.date_added = date_added;
+        this.date_added = date_added;    
         this.release_year = release_year;
         this.rating = rating;
         this.duration = duration;
-        this.listed_in = listed_in;
-    }
+        this.listed_in = new ArrayList<>(listed_in);
+}
 
     // Getters e Setters
     public String getShow_id() {return Show_id;}
@@ -73,8 +71,8 @@ public class Show{
     public String getDirector() {return director;}
     public void setDirector(String director) {this.director = director;}
     
-    public String[] getCast() {return cast;}
-    public void setCast(String[] cast) {this.cast = cast;}
+    public ArrayList<String> getCast() { return new ArrayList<>(cast); }
+    public void setCast(ArrayList<String> cast) { this.cast = new ArrayList<>(cast); }
     
     public String getCountry() {return country;}
     public void setCountry(String country) {this.country = country;}
@@ -91,12 +89,12 @@ public class Show{
     public String getDuration() {return duration;}
     public void setDuration(String duration) {this.duration = duration;}
     
-    public String[] getListed_in() {return listed_in;}
-    public void setListed_in(String[] listed_in) {this.listed_in = listed_in;}  
+    public ArrayList<String> getListed_in() { return new ArrayList<>(listed_in); }
+    public void setListed_in(ArrayList<String> listed_in) { this.listed_in = new ArrayList<>(listed_in); }
 // ---------------------------------------------------------------------------------------------------- //
 //Funções
 
-    //Função transformar uma string em um objeto
+    //+-+-+--+-+-+-+ Função transformar uma string em um objeto +-+-+--+-+-+-+
     public static Show TrasInObj(String linha){
         Show showzinho = new Show();
         int i=0;        
@@ -150,30 +148,108 @@ public class Show{
 		showzinho.setType(atrib.get(1));
 		showzinho.setTitle(atrib.get(2));
 		showzinho.setDirector(atrib.get(3));
-		//showzinho.setCast(splitArrays(atrib.get(4)));
+		showzinho.setCast(Separa(atrib.get(4)));
 		showzinho.setCountry(atrib.get(5));
-		//showzinho.setDate_added(atrib.get(6));
+		
+        //Tratamento da data
         try {
             if(!atrib.get(6).equals("NaN")) {
-                SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM d, yyyy"); 
-                showzinho.setDate_added(dateFormat.parse(atrib.get(6)));
-            } 
+                String dataNormalizada = atrib.get(6).replace(",", "").trim();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM dd yyyy", Locale.ENGLISH);
+                showzinho.setDate_added(dateFormat.parse(dataNormalizada));
+            } else {
+                showzinho.setDate_added(null);
+            }
         } catch (Exception e) {
             System.err.println("Erro ao converter data: " + atrib.get(6));
             showzinho.setDate_added(null);
         }
+
 		showzinho.setRelease_year(Integer.parseInt(atrib.get(7)));
 		showzinho.setRating(atrib.get(8));
 		showzinho.setDuration(atrib.get(9));
-		//showzinho.setLISTED_IN(atrib.get(10));
+		showzinho.setListed_in(Separa(atrib.get(10)));
 		
-         
-
-
-
         return showzinho;
     }
-    //Função ler arquivo csv
+    
+    //+-+-+--+-+-+-+ Função para separar elementos por virgula +-+-+--+-+-+-+
+    public static ArrayList<String> Separa(String s){
+        ArrayList<String> separado = new ArrayList<>();
+        int i = 0;
+
+        while (i < s.length()) {
+            if (s.equals("NaN")) {
+                separado.add("NaN");
+                break;
+            } else {
+                StringBuilder aux = new StringBuilder();
+                while (i < s.length() && s.charAt(i) != ',') {
+                    aux.append(s.charAt(i));
+                    i++;
+                }
+                separado.add(aux.toString());
+            }
+            i += 2;
+        }
+        Collections.sort(separado);
+        return separado;
+    }
+    
+    //+-+-+--+-+-+-+Função para imprimir a serie desejada+-+-+--+-+-+-+
+    public static void ImprimirPorId(String id, ArrayList<Show> show){
+        SimpleDateFormat outputFormat = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH);//Tratamento da data
+        for(int i=0; i< show.size();i++){
+            if(show.get(i).getShow_id().equals(id)){
+
+                String dataFormatada = (show.get(i).date_added != null) ? outputFormat.format(show.get(i).date_added) : "NaN"; //Tratamento da data
+                System.out.print("=> " + show.get(i).Show_id);
+                System.out.print(" ## ");
+                System.out.print(show.get(i).title);
+                System.out.print(" ## ");
+                System.out.print(show.get(i).type);
+                System.out.print(" ## ");
+                System.out.print(show.get(i).director);
+                System.out.print(" ## ");
+                System.out.print(show.get(i).cast);
+                System.out.print(" ## ");
+                System.out.print(show.get(i).country);
+                System.out.print(" ## ");
+                System.out.print(dataFormatada);
+                System.out.print(" ## ");
+                System.out.print(show.get(i).release_year);
+                System.out.print(" ## ");
+                System.out.print(show.get(i).rating);
+                System.out.print(" ## ");
+                System.out.print(show.get(i).duration);
+                System.out.print(" ## ");
+                System.out.print(show.get(i).listed_in); 
+                System.out.println(" ## ");
+
+            }
+        }
+         
+    }
+
+    //+-+-+--+-+-+-+ Funação para clonar +-+-+--+-+-+-+
+    public Show clone() {
+        Show clone = new Show();
+        clone.setShow_id(this.getShow_id());
+        clone.setType(this.getType());
+        clone.setTitle(this.getTitle());
+        clone.setDirector(this.getDirector());
+        clone.setCast(new ArrayList<>(this.getCast()));
+        clone.setCountry(this.getCountry());
+        clone.setDate_added(this.getDate_added() != null ? new Date(this.getDate_added().getTime()) : null);
+        clone.setRelease_year(this.getRelease_year());
+        clone.setRating(this.getRating());
+        clone.setDuration(this.getDuration());
+        clone.setListed_in(new ArrayList<>(this.getListed_in()));
+        return clone;
+    }
+
+   
+    //+-+-+--+-+-+-+ Função ler arquivo csv +-+-+--+-+-+-+
     public static ArrayList<Show> Ler(){
         String linha = "";
 		int aux = 0;
@@ -184,7 +260,7 @@ public class Show{
 			while ((linha = br.readLine()) != null) {
 
                 if(aux >= 1){
-					System.out.println(linha + '\n');
+					//System.out.println(linha + '\n');
 					show.add(TrasInObj(linha));
 				}
 
@@ -194,7 +270,7 @@ public class Show{
 			}
             br.close();
 		} catch (Exception e) {
-			System.out.println("Erro!!");
+			System.out.println("Erro ao ler arquivo!!");
 		}
 		return show;
         
@@ -207,11 +283,19 @@ public class Show{
       
         Scanner sc = new Scanner(System.in);
         ArrayList <Show> show = Ler();
-        System.out.println("Digite o id para retornar o titulo:");
-		int id = sc.nextInt();
-		System.out.println(show.get(id - 1).title);
+     
+        //System.out.println("Digite o id para pesquisar: ");
+		String id = sc.nextLine();
+        while (!id.equals("FIM")) {
+            ImprimirPorId(id, show);
+            //System.out.println("Digite o id para pesquisar: ");
+            id = sc.nextLine();
+          
+        }
         
-           
+		
+
+            
         sc.close();
     }
 }
